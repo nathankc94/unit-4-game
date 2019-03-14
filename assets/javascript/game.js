33aa03ca04bf9bee1ef19e5c18e241d5488dc2e3
alongkot <nathankc94@gmail.com>
2019-03-14 18:09:21 -0500

    adding varibles

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+// 1. create varribles
+    var winningNumber = "";
+    var totalScore = "";
+    var crystal1 = "";
+    var crystal2 = "";
+    var crystal3 = "";
+    var crystal4 = "";
+    var wins = 0;
+    var losses = 0;
+    
+
+// 2. generate winning number
+
+
+
+// 3. generate crystals value
+
+// 4.reset game
+
+// 5. update game
+
+// 6. capture user input
+
+// 7. determine if win or lose 
+
+
+
+

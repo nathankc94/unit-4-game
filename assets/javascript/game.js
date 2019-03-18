8861eddbae65dd2d17ef479ca55148751a20b5b9
alongkot <nathankc94@gmail.com>
2019-03-18 10:48:30 -0500

    finals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,108 @@
-// 1. create varribles
-var crystal1 = 0;
-var crystal2 = 0;
-var crystal3 = 0;
-var crystal4 = 0;
 
-var winningNumber = 0;
-var totalScore = 0;
+$(document).ready(() => {
+    // 1. create varribles //////////////////////////////
 
-var wins = 0;
-var losses = 0;
-    
-    
+    var crystal1 = Math.floor(Math.random() * 12) + 1;;
+    var crystal2 = Math.floor(Math.random() * 12) + 1;;
+    var crystal3 = Math.floor(Math.random() * 12) + 1;;
+    var crystal4 = Math.floor(Math.random() * 12) + 1;;
 
-// 2. generate winning number
+    var winningNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+    $("#targetNumber").html(winningNumber);
+
+    var totalScore = 0;
+    $("#totalScore").html(totalScore);
+
+    var wins = 0;
+    var losses = 0;  
+
+    //  2. user input ///////////////////////////////////
+    $('#crystal1').click(() => {
+        totalScore = totalScore + crystal1;
+        $('#totalScore').text(totalScore);
+        if (totalScore === winningNumber) {
+            win();
+            reset();
+            resetTwo();
+        } else if (totalScore > winningNumber) {
+            lose();
+            reset();
+            resetTwo();
+        }
+    });
+
+    $('#crystal2').click(() => {
+        totalScore = totalScore + crystal2;
+        $('#totalScore').text(totalScore);
+        if (totalScore === winningNumber) {
+            win();
+            reset();
+            resetTwo();
+        } else if (totalScore > winningNumber) {
+            lose();
+            reset();
+            resetTwo();
+        }
+    });
+
+    $('#crystal3').click(() => {
+        totalScore = totalScore + crystal3;
+        $('#totalScore').text(totalScore);
+        if (totalScore === winningNumber) {
+            win();
+            reset();
+            resetTwo();
+        } else if (totalScore > winningNumber) {
+            lose();
+            reset();
+            resetTwo();
+        }
+    });
+    $('#crystal4').click(() => {
+        totalScore = totalScore + crystal4;
+        $('#totalScore').text(totalScore);
+        if (totalScore === winningNumber) {
+            win();
+            reset();
+            resetTwo();
+        } else if (totalScore > winningNumber) {
+            lose();
+            reset();
+            resetTwo();
+        }
+    });
+
+    // 3. count wins and losses //////////////////////////////
+
+    function win() {
+        wins = wins + 1;
+        $('#wins').text(wins);
+        resetTwo();
+        reset();
+    }
+
+    function lose() {
+        losses = losses + 1;
+        $('#losses').text(losses);
+        resetTwo();
+        reset();
+    }
+
+    // 4. reset game ////////////////////////
+    function reset() {
+        totalScore = 0;
+        $('#totalScore').text(totalScore);
+    }
+
+    function resetTwo() {
+        winningNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+        $('#targetNumber').html(winningNumber);
+        crystal1 = Math.floor(Math.random() * 12) + 1;
+        crystal2 = Math.floor(Math.random() * 12) + 1;
+        crystal3 = Math.floor(Math.random() * 12) + 1;
+        crystal4 = Math.floor(Math.random() * 12) + 1;
+    }
 
 
-
-// 3. generate crystals value
-
-// 4.start game
-function getRandom (min, max){
- return Math.floor(Math.random() * (max - min + 1)) + min; 
-}
-
-var generateNums = function (){
-    var totalScore = 0; 
-    winningNumber = getRandom.apply(19, 120);
-
-    crystal1 = getRandom(1,12);
-    crystal2 = getRandom(1,12);
-    crystal3 = getRandom(1,12);
-    crystal4 = getRandom(1,12); 
-
-    console.log(winningNumber); 
-    console.log(crystal1); 
-    console.log(crystal2); 
-};
-
-// 5. update game
-
-// 6. capture user input
-// startGame();
-
-$("#red").click(function(){
-    alert("12");
 });
-$("#white").click(function(){
-    alert("10");
-});
-$("#purple").click(function(){
-    alert("1");
-});
-$("#green").click(function(){
-    alert("3");
-});
-
-// 7. determine if win or lose 
-
-
-
 
